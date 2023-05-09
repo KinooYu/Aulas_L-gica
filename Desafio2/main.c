d129154aa0b897b7029ded9aef04245f6174c834
KinoYu <educoloni12@gmail.com>
2023-05-09 00:24:32 -0400

    Desafio2 update
    Apenas melhorei o código do desafio 2

--- Desafio2/main.c
+++ Desafio2/main.c
@@ -8,13 +8,13 @@ int main() {
     //Iniciando a funcao de aleatoriedade
     srand(time(NULL));/*Utilizado para iniciar a seed do srand com um numero de segundos que vem desde 1 de jan de 1970
                                   assim deixando verdadeiramente aleatorio o numero do rand*/
-    aleatorio = rand() % 100;
+    aleatorio = rand() % 1000 + 1;
 
     printf("%d \n", aleatorio);//Só para eu saber qual o numero aleatorio para fins de teste
 
     while (contador)//Fazendo um contador com o while, enquanto o a condiçao do if não for verdadeira ele vai continuar pedindo um numero a ele
     {
-        printf("Digite um numero:");
+        printf("Digite um numero entre 1 e 1000:");
         scanf("%d", &num);
         if (num == aleatorio){//Vai ser executado ao o numero do usuario ser igual ao num aleatorio
             printf("Acertouuuu!\n");
@@ -23,29 +23,39 @@ int main() {
         else if (num > aleatorio){
             printf("O numero digitado é maior, %dº tentativa  continue tentando!\n", contador);
         }
+        else if (contador > 15){
+            printf("Vocẽ atingiu o limite de tentativas!\n");
+            break;
+        }
         else{
             printf("O numero digitado é menor, %dº tentativa continue tentando!\n", contador);
         }
         contador++;
     }
 
-    switch (contador) { //Usando o switch para teste, já que não uso muito ele mesmo, interessante como salva algumas linhas doque a versão anterior com IF
-        case 1 ... 5:
-            printf("Sua classificação foi excelente, você acertou de %dº!!", contador);
-            break;
-        case 6 ... 7:
-            printf("Sua classificação foi bom, você acertou de %dº!!", contador);
-            break;
-        case 8 ... 12:
-            printf("Sua classificação foi normal, você acertou de %dº!!", contador);
-            break;
-        case 13 ... 14:
-            printf("Sua classificação foi ruim, você acertou de %dº!!", contador);
-            break;
-        default:
-            printf("Sua classificação foi trágica, você acertou de %dº!!", contador);
-            break;
+    if(contador <= 15);
+    {
+        switch (contador) { //Usando o switch para teste, já que não uso muito ele mesmo, interessante como salva algumas linhas doque a versão anterior com IF
+            case 1 ... 5:
+                printf("Sua classificação foi excelente, você acertou de %dº!!", contador);
+                break;
+            case 6 ... 7:
+                printf("Sua classificação foi bom, você acertou de %dº!!", contador);
+                break;
+            case 8 ... 12:
+                printf("Sua classificação foi normal, você acertou de %dº!!", contador);
+                break;
+            case 13 ... 14:
+                printf("Sua classificação foi ruim, você acertou de %dº!!", contador);
+                break;
+            case 15:
+                printf("Sua classificação foi trágica, você acertou de %dº!!", contador);
+                break;
+            default:
+                printf("Você não acertou nenhum");
+                break;
+        }
     }
-
     return 0;
 }
+
